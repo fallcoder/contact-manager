c529b6f4d4235c97269e29a318d9e6d50e518298
fallcoder <mouhamedfall056@gmail.com>
2024-07-06 15:07:28 +0000

    added two methods for search contact

--- js/Phonebook.js
+++ js/Phonebook.js
@@ -10,11 +10,26 @@ export default class Phonebook {
 
     // méthode pour ajouter un contact a la liste
     AddContact(name, number) {
+        // vérification des types d'arguments
+        if(typeof name !== 'string' || typeof number !== 'number') {
+            throw new Error("Invalid arguments: name must be a string and must be a number");
+        }
+
         const contact = new Contact(name, number)
         this.contacts.push(contact)
     }
 
-    // méthode pour affcicher tous les contacts dans la console
+    // méthode pour rechercher un contact par son nom
+    findContactByName(name) {
+        return this.contacts.find(contact => contact.name === name)
+    }
+
+    // méthode pour rechercher un contact par son numéro
+    findContactByNumber(number) {
+        return this.contacts.find(contact => contact.number === number)
+    }
+
+    // méthode pour afficher tous les contacts dans la console
     printAllContacts() {
         this.contacts.forEach(contact => contact.print())
     }
